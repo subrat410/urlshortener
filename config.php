45dd6158f39dbb86e6da079772d03ed81b91d2ec
subrat410 <subratsaxena9@gmail.com>
2019-03-04 16:00:13 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-	$config['db_host'] = 'us-cdbr-iron-east-03.cleardb.net';
-	$config['db_user'] = 'b25bb9feeb70ad';
-	$config['db_pass'] = '6c187c3e';
-	$config['db_name'] = 'heroku_52ead61bfd0a60f';
+	$config['db_host'] = '';
+	$config['db_user'] = '';
+	$config['db_pass'] = '';
+	$config['db_name'] = '';
 	
 	foreach($config as $k => $v){
 		define(strtoupper($k),$v);
